038108a6b4d24ac77ccdd6567a2fa5d46f406e06
RIKUTO KURODA <rikutokuroda@MacBook-Pro.local>
2021-08-29 18:09:54 +0900

    コメントの変更と、メソッドの引数の追加及びリストの追加の値の変更

--- DB/MYSQL/fomatter.php
+++ DB/MYSQL/fomatter.php
@@ -1,17 +1,19 @@
 <?php 
 
-// Class that converts an object into a list
+// Generate a list of column names
 class DbFormatter {
 	private $db_list = [];
 
 	public function getDbList() {
 		return $this->db_list;
 	}
-	public function setDbList($db_list) {
-		if(!is_array($db_list) && !is_string($db_list)) {
+	public function setDbList($db_lists, $column_name) {
+		if(!is_array($db_lists) && !is_string($db_lists)) {
 			throw new Exception("Error Processing Request");
 		}
-		$this->db_list[] = $db_list;
+		foreach($db_lists as $db_columns) {
+			$this->db_list[] = $db_columns[$column_name];
+		}
 	}
 }
 
