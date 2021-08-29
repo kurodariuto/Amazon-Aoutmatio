377f21cee2b6d51ecbcde162079292896e0d49a4
RIKUTO KURODA <rikutokuroda@MacBook-Pro.local>
2021-08-30 01:16:13 +0900

    ファイルパスの追加と、ページ遷移の実行処理の追加

--- main.php
+++ main.php
@@ -2,6 +2,7 @@
 
 // HACH：Better to change the directory path to a constant
 require_once __DIR__ . '/DB/MYSQL/sql.php';
+require_once __DIR__ . '/DB/MYSQL/fomatter.php';
 require_once __DIR__ . '/UI/Amazon/search.php';
 require_once __DIR__ . '/vendor/autoload.php';
 use Facebook\WebDriver\Chrome\ChromeOptions;
@@ -18,6 +19,14 @@ $options->addArguments([
 ]);
 
 $mysql = new MYSQL();
-$mysql->select_all();
+$db_formatter = new DbFormatter();
+$db_formatter->setDbList($mysql->select_all(), 'asin');
+$db_formatter_list = $db_formatter->getDbList();
+
+$search_action = new SearchAction();
+$search_action->setUrlList($db_formatter_list);
+
+// XXX：The link below does not work properly due to age restrictions
+$search_action->serach_all($search_action->getUrlList());
 
  ?>
